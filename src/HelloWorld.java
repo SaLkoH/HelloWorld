4f30822c04fd0e6cc769238bd58f330f7b602800
HokLaS <salkoh@mail.ru>
2020-09-25 21:08:30 +0300

    Practice #4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int i = 666;
+        System.out.println(Integer.toBinaryString(i));
+        int in = 1100001001;
+        System.out.println(Integer.parseInt(String.valueOf(in), 2));
+
     }
 }
