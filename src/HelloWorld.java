d32dc3d3b5f8fffb5c013e4afbe6523b39217a01
HokLaS <salkoh@mail.ru>
2020-09-26 15:21:17 +0300

    Practice #4 and #5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -6,6 +7,12 @@ public class HelloWorld {
         System.out.println(Integer.toBinaryString(i));
         int in = 1100001001;
         System.out.println(Integer.parseInt(String.valueOf(in), 2));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число в бинарном формате: ");
+        String bin = sc.nextLine();
+        int binNum = Integer.parseInt(bin, 2);
+
+        System.out.println(binNum);
 
     }
 }
