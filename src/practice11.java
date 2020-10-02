ccc6fcd0662c224e3677f00de00d6eb392e0eed1
HokLaS <salkoh@mail.ru>
2020-10-02 20:19:59 +0300

    Practice #11

--- src/practice11.java
+++ src/practice11.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class practice11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Введите целое число: ");
-        int s = input.nextInt();
-        String str = String.format("%d",  s);
+        System.out.println("Введите строку с клавиатуры: ");
+        String s = input.nextLine();
+        String str = String.format("%s",  s);
         input.close();
         System.out.println("Строковая переменная: " + str);
     }
