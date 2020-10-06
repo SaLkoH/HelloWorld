d5227a5de695723d0b312f23767d4fa7060ea342
HokLaS <salkoh@mail.ru>
2020-10-06 13:05:14 +0300

    Practice #16

--- src/practice16.java
+++ src/practice16.java
@@ -1,8 +1,9 @@
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class practice16 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("practice16.txt");
         Scanner scan = new Scanner(fr);
 
