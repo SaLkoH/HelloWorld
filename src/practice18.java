360989d4e7618a707be7f10b24d9f03ec6c70a85
HokLaS <salkoh@mail.ru>
2020-10-13 20:28:14 +0300

    Practice #18 fixed after comments

--- src/practice18.java
+++ src/practice18.java
@@ -3,28 +3,23 @@ import java.io.*;
 public class practice18 {
     public static void main(String[] args) throws IOException {
         String s;
-        BufferedReader br = new BufferedReader(new FileReader("practice18.txt"));
-        while ((s = br.readLine()) != null) {
-            System.out.println(s);
-        }
-
-
-        LineNumberReader linenumber = new LineNumberReader(new FileReader("practice18.txt"));
         int ln = 0;
-        while (linenumber.readLine() != null) {
-            ln++;
+        BufferedReader br = new BufferedReader(new FileReader("practice18.txt"));
+        LineNumberReader linenumber = new LineNumberReader(new FileReader("practice18.txt"));
+        while ((s = br.readLine()) != null & linenumber.readLine() != null) {
+            System.out.println(s);
+                ln++;
         }
         System.out.println("Количество строк в файле: " + ln);
+
 
         BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
         FileWriter fw = new FileWriter("practice18.txt");
         String text;
-        int i = 0;
-        while (i < ln) {
-            System.out.print("Измените " + (i+1) + " строку: ");
+        for (int i = 0; i < ln; i++) {
+            System.out.print("Измените " + (i + 1) + " строку: ");
             text = br1.readLine();
             fw.write(text + "\n");
-            i++;
             fw.flush();
         }
     }
